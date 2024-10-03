387a929580d797f3e3cedc77ebfdd04bee15a3b6
JonathanParra467 <jparra1786@students.mchenry.edu>
2024-10-03 18:43:48 -0500

    Add files via upload

---
+++ dictinaries.py
@@ -0,0 +1,35 @@
+def main():
+    nato_alphabet = {
+        "A": "Alpha",
+        "B": "Bravo",
+        "C": "Charlie",
+        "D": "Delta",
+        "E": "Echo",
+        "F": "Foxtrot",
+        "G": "Golf",
+        "H": "Hotel",
+        "I": "India",
+        "J": "Juliette",
+        "K": "Kilo",
+        "L": "Lima",
+        "M": "Mike",
+        "N": "November",
+        "O": "Oscar",
+        "P": "Papa",
+        "Q": "Quebec",
+        "R": "Romeo",
+        "S": "Sierra",
+        "T": "Tango",
+        "U": "Uniform",
+        "V": "Victor",
+        "W": "Whiskey",
+        "X": "X - Ray",
+        "Y": "Yankee",
+        "Z": "Zulu",
+    }
+    word = input("Input a word here: ").upper()
+    for i in word:
+        print(nato_alphabet[i])
+
+
+main()
